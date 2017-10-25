73f84f07ec1fa7284bcd15ca895edfcafbff0d14
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-10-25 09:28:15 +0000

    a

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -380,7 +380,7 @@ $scenes = $scenes
           if(Photo::where('scene_id',$scene->scene_id)->whereNotNull('data')->exists()){
           $data['thumb'][] = Photo::where('scene_id',$scene->scene_id)
                                   ->whereNotNull('data')
-                                  ->select('encode(data::bytea,"escape") escdata','mime')
+                                  //->select('encode(data::bytea,"escape") escdata','mime')
                                   //->orderByRaw("RAND()")
                                   ->first();
           }
