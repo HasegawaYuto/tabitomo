58d62af4391ffe2a4f00a122915136d7ac1bd6e5
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-10-27 07:53:59 +0000

    a

--- app/Http/Controllers/ItemPostController.php
+++ app/Http/Controllers/ItemPostController.php
@@ -387,7 +387,7 @@ class ItemPostController extends Controller
     }
     public function deleteScene($scene_id){
         if(Photo::where('scene_id',$scene_id)->exists()){
-            $thephotos = where('scene_id',$scene_id)->get();
+            $thephotos = Photowhere('scene_id',$scene_id)->get();
             foreach($thephotos as $thephoto){
                 $filename = explode('upload',$thephoto->path);
                 $thephotoextension = explode('.',$filename[1]);
