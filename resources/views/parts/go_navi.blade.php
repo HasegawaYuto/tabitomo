8f8fb8002c10838f2893e9968512a350be157ac9
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-10-27 09:32:09 +0000

    a

--- resources/views/parts/go_navi.blade.php
+++ resources/views/parts/go_navi.blade.php
@@ -5,7 +5,7 @@ $browser=((strpos($ua,'iPhone')!==false)||(strpos($ua,'iPod')!==false)||(strpos(
 @if($browser!='sp')
     <a href="https://maps.google.com/?daddr={{$scene->lat}},{{$scene->lng}}&ll={{$scene->lat}},{{$scene->lng}}&z=10" class="btn btn-danger btn-block">ここへ行く</a>
 @else
-    <a href="comgooglemaps//?daddr={{$scene->lat}},{{$scene->lng}}&directionsmode=driving" class="btn btn-danger col-xs-4"><i class="fa fa-car" aria-hidden="true"></i></a>
+    <a href="comgooglemaps://?daddr={{$scene->lat}},{{$scene->lng}}&directionsmode=driving" class="btn btn-danger col-xs-4"><i class="fa fa-car" aria-hidden="true"></i></a>
     <a href="comgooglemaps://?daddr={{$scene->lat}},{{$scene->lng}}&directionsmode=transit" class="btn btn-info col-xs-4"><i class="fa fa-subway" aria-hidden="true"></i></a>
     <a href="comgooglemaps://?daddr={{$scene->lat}},{{$scene->lng}}&directionsmode=walking" class="btn btn-warning col-xs-4"><i class="fa fa-male" aria-hidden="true"></i></a>
 @endif
