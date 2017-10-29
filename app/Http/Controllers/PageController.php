61499ed508bceb9c07b1d431e422a814b8cf4398
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-10-29 08:08:20 +0000

    search

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -156,7 +156,7 @@ $scenes = $scenes
       }
       if(!$genreNot){
           //$genres = implode("",$request->genre);
-          $scenes = $scenes->where('genre','ERGEXP','[A]*');
+          $scenes = $scenes->where('genre','REGEXP','[A]*');
           //foreach($request->genre as $genre){
           //    $scenes = $scenes->where('genre','like','%'.$genre.'%');
           //}
