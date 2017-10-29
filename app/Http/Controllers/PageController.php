4fe9d11c3efa63db04341cec6890ced8f47f6e67
yutohasegawa <buenas_tardes@re.commufa.jp>
2017-10-29 04:43:54 +0000

    search

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -156,7 +156,7 @@ $scenes = $scenes
       }
       if(!$genreNot){
           $genres = implode("",$request->genre);
-          $scenes = $scenes->where('genre','~','[^ABC]*');
+          $scenes = $scenes->where('genre','~','[^BC]*');
           //foreach($request->genre as $genre){
           //    $scenes = $scenes->where('genre','like','%'.$genre.'%');
           //}
